034371096d702ad90e30cb531f278f5bb97c4ec3
Mubby007 <Mubarakadebayo06@gmail.com>
2023-09-12 11:23:15 +0100

    my react app

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  
+function App() {
+    return (
+      <div>
+        <nav>
+          <div className='logo'>
+            <i class='bx bxl-react logo'></i><span>ReactFacts</span>
+            
+             </div>
+          
+          <div className='nav-item'>
+             <h2>React course Project-1</h2> 
+             </div>
+        </nav>
+  
+        <main>
+  
+          <div>
+          <div>
+            <h1>Fun facts about react </h1>
+          </div>
+  
+          <div>
+  
+            <ul>
+              <li> <span>Was first released in 2013</span> </li>
+              <li> <span>Was originally created by Jordan Walke</span></li>
+              <li> <span>Has well over 100K stars on GitHub</span></li>
+              <li> <span>Is maintained by Facebook</span></li>
+              <li> <span>Powers thousands of enterprise apps, including mobile apps</span></li>
+            </ul>
+          </div>
+  
+  
+          </div>
+                </main>
+      </div>
+    )
+  
+  
+    }
+  
+  
+  
+  
+  export default App
+  
+  //🎉 Just launched my very first React website! 🚀✨ Excited to share my journey into web development. Check it out and let me know your thoughts. Big shoutout to the amazing React community for all the support and resources! #React #WebDevelopment #NewbieCoder
+  
